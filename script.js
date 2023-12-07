8ea9e5610976c0556de7e7153d8a0aa6f65eabf2
manopickax <rnrrbs0202@gmail.com>
2023-12-07 17:47:52 +0900

    ad : 기도횟수 카운팅 기능 추가

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+document.getElementById("prayButton").addEventListener("click", function() {
+    incrementPrayerCount();
+    redirectToOutputPage();
+});
+
 function redirectToOutputPage() {
     window.location.href = "output.html";
 }
+
+function incrementPrayerCount() {
+    let prayerCount = localStorage.getItem("prayerCount") || 0;
+    prayerCount++;
+    localStorage.setItem("prayerCount", prayerCount);
+}
+
