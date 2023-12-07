4ba9f2b2b451e8d0cdff0470041cb55bbc9469b0
manopickax <rnrrbs0202@gmail.com>
2023-12-07 18:13:14 +0900

    add : 랜덤 메시지 출력 기능 추가

--- output.js
+++ output.js
@@ -8,9 +8,28 @@ function redirectToIndexPage() {
 
 document.addEventListener("DOMContentLoaded", function() {
     displayPrayerCount();
+    displayRandomMessage();
 });
 
 function displayPrayerCount() {
     let prayerCount = localStorage.getItem("prayerCount") || 0;
     document.getElementById("prayerCount").textContent = prayerCount;
+}
+
+function displayRandomMessage() {
+    const messages = [
+        "신의 가호가 함께 하기를",
+        "평안과 기쁨이 당신과 함께 하길",
+        "건강과 행복이 당신을 감싸길",
+        "사랑과 이해가 너를 둘러싸기를"
+    ];
+
+    const randomIndex = Math.floor(Math.random() * messages.length);
+    const randomMessage = messages[randomIndex];
+
+    const messageElement = document.createElement("p");
+    messageElement.textContent = randomMessage;
+
+    const centerContainer = document.querySelector(".center-container");
+    centerContainer.insertBefore(messageElement, centerContainer.firstChild);
 }
